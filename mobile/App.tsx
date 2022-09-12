ead6dd50dd27d4c356a847db3b95643888dcbc9c
Marcelo M Oliveira <marcelomra.oliveira@gmail.com>
2022-09-12 14:33:58 -0300

    Adicionando API; Adicionando projeto mobile; Adicionando projeto web;

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+
+interface ButtonProps {
+  title: string
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Fala tu parça</Text>
+      <Button title='batata' />
+      <StatusBar style="auto" backgroundColor='purple'/>
     </View>
-  );
+  )
+}
+
+function Button(props: ButtonProps) {
+  return (
+    <TouchableOpacity>
+      <Text style={styles.title}>
+        {props.title}
+      </Text>
+    </TouchableOpacity>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    color: '#fff',
+    fontSize: 22
+  }
 });
